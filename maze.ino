52ad5ec125f32017ffcfc3a57d94ee68324520c8
ericskh02 <42265526+ericskh02@users.noreply.github.com>
2020-09-21 15:30:49 +0800

    Update maze.ino

--- maze.ino
+++ maze.ino
@@ -16,7 +16,7 @@ void turnRight(int time, int speed){
   travelled.push_back(2);
 }
 
-void shortestPathCheck(int mode){ 
+void shortestPathCheck(int mode){ //for saving the shortest path of maze travel
   switch(mode){
     case 0:
       if(travelled.back()==0){
@@ -42,19 +42,19 @@ void maze(bool leftMode){
   
   while(!mazeSolved){
     if(leftMode){
-      if(left_distance<10){
+      if(left_distance<10){ // First rule: if there is road to left
         turnLeft(2000,255);
         moveForward(1000,255);
         if(!shortestpath){
           shortestPathCheck(1);
         }
-      } else if (front_distance<10){
+      } else if (front_distance<10){ // Second rule: if there is road forward
         moveForward(100,255);
         travelled.push_back(0);
         if(!shortestpath){
           shortestPathCheck(0);  
         }
-        } else {
+        } else { //Third rule: if there is no road for left and forward
           turnRight(2000,255);
           if(travelled.back()==2){
             shortestpath = false;
@@ -67,17 +67,17 @@ void maze(bool leftMode){
         }  
     }
   }
-  vector<string>::iterator it;
+  vector<string>::iterator it; // print the shortest route at the end
   for(it = travelled.begin();it!=travelled.end();it++){
     Serial.print(*it);
   }
 }
 
-void setup() {
+void setup() { // Initialize
   Serial.begin(9600);
 }
 
-void loop() {
+void loop() { // Main, need to add function to start the maze algorithm later
   left_distance = 0;
   front_distance = 0;  
 }
