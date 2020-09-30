c9af314c5313694ddebab0752d2117c66a8ce211
ericskh02 <42265526+ericskh02@users.noreply.github.com>
2020-09-30 20:50:39 +0800

    Update ultrasonic.ino

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,23 +1,44 @@
-#include <NewPing.h>
- 
-#define TRIGGER_PIN  2
-#define ECHO_PIN     3
-#define TRIGGER_PIN_2 4
-#define ECHO_PIN_2 5
-#define MAX_DISTANCE 200
- 
-NewPing sonar1(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
-void setup() {
-  Serial.begin(115200);
+#define TRIG_1 12
+#define ECHO_1 A0
+#define TRIG_2 
+#define ECHO_2
+#define TRIG_3
+#define ECHO_3
+
+double distance;
+double duration;
+
+void setup(){
+  pinMode (TRIG_1, OUTPUT);
+  pinMode (ECHO_1, INPUT);
+  pinMode(TRIG_2,OUTPUT);
+  pinMode(ECHO_2,INPUT);
+  pinMode(TRIG_3,OUTPUT);
+  pinMode(ECHO_3,INPUT);
+  Serial.begin (9600);
 }
- 
-void loop() {
+
+void loop(){
+  digitalWrite(TRIG_1, LOW);
+  digitalWrite(TRIG_2, LOW);
+  digitalWrite(TRIG_3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_1, HIGH);
+  digitalWrite(TRIG_2, HIGH);
+  digitalWrite(TRIG_3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_1, LOW);
+  digitalWrite(TRIG_2, LOW);
+  digitalWrite(TRIG_3, LOW);
+  delayMicroseconds(2);
+  duration_1 = pulseIn(ECHO_1, HIGH);
+  duration_2 = pulseIn(ECHO_2, HIGH);
+  duration_3 = pulseIn(ECHO_3, HIGH);
+  distance_1 = duration_1*0.034/2;
+  distance_2 = duration_2*0.034/2;
+  distance_3 = duration_3*0.034/2;
+  Serial.println(distance_1);
+  Serial.println(distance_2);
+  Serial.println(distance_3);
   delay(1000);
-  Serial.print("Ping Sensor1: ");
-  Serial.print(sonar1.ping_cm());
-  Serial.println("cm");
-  Serial.print("Ping Sensor2: ");
-  Serial.print(sonar2.ping_cm());
-  Serial.println("cm");
 }
